e3186e43b04f39e28da1ed678d8a309789a1567d
jiz225 <jzc7523@outlook.com>
2025-05-29 10:53:25 -0400

    20250529 cleanup

--- benchmark/wrapper_runtime.sh
+++ benchmark/wrapper_runtime.sh
@@ -1,4 +1,4 @@
-for num_t in "12" "24" "36" "48"; do
-    bash /Download/UMI-nea/benchmark/compare_tools.sh 12 0.995 10000 100 3 1 1-1-40 0 0 $num_t 2 UMI-nea,UMIC-seq,calib
-    bash /Download/UMI-nea/benchmark/compare_tools.sh 50 0.99 10000 100 3 1 1-1-1 0 0 $num_t 2 UMI-nea,UMIC-seq,calib
+for num_t in "8" "12" "24" "36" "48"; do
+    bash /Download/UMI-nea/benchmark/compare_tools.sh 12 0.005 10000 100 3 1 1-1-40 0 0 $num_t 2 UMI-nea,UMIC-seq,calib
+    bash /Download/UMI-nea/benchmark/compare_tools.sh 25 0.01 10000 100 3 1 1-1-1 0 0 $num_t 2 UMI-nea,calib
 done
